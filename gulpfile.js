472827747cec472941bd224ab428bb8649cbc0d1
David <kansas@inkwhiskey.com>
2015-08-06 15:52:39 -0400

    added gulp type icons etc

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+// Sass styles
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+// Watch task
+
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['sass']);
+});
